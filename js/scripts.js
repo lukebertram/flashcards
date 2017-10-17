ef7f3237d17deacf0575908eddcf4e4c90f0ad00
Cat Janowitz and Luke Bertram <cat@>
2017-10-17 11:19:31 -0700

    add toggle function to term list links

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#js-link").click(function() {
+    // $("#javascript-card").removeClass("hidden");
+    $("#javascript-card").toggle("hidden");
+  });
+});
+
+
+$(document).ready(function() {
+  $("#jquery-link").click(function() {
+    // $("h3#jquery-card").removeClass("hidden");
+    $("#jquery-card").toggle("hidden");
+  });
+});
+
+$(document).ready(function() {
+  $("#attrib-link").click(function() {
+    // $("h3#attributes-card").removeClass("hidden");
+    $("#attributes-card").toggle("hidden");
+  });
+});
